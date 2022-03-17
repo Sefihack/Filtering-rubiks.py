7e3655bf20e056d08df2378279a4c5da27f0ba9a
SefiHacker <100835730+SefiHacker@users.noreply.github.com>
2022-03-17 20:30:26 +0330

    Update and rename README.md to Filtering'rubika.py

---
+++ Filtering'rubika.py
@@ -0,0 +1,57 @@
+import os
+import sys
+import time
+
+Filtering1 = 1
+ib = 2
+exit = 3
+class color : 
+    RED = '\033[91m'
+    Green = '\033[m32'
+z = """ 
+
+        - Yosef | Hanger kabir
+
+[]_______________________________________[] 
+
+""" 
+print(color.RED + (z)) 
+print(color.Green + "[1]: Filtering rubika") 
+print(color.Green + "[2]: SeFi") 
+print(color.Green + "[3]ID") 
+
+filteringg = input(">>") 
+s = """ 
+
+          - SeFi | hacker
+[]__________________________________[]
+
+
+
+
+-
+--
+---
+----
+-----
+------
+-------          
+------
+----
+---
+--
+-
+
+""" 
+if filteringg == Filtering1:
+  print(color.RED + (s)) 
+  print(color.Green + "(/5.7.3.6/t////H/h/6.7.3.0.2.4.4.6.7.1.3.2.6.8.4.5.6.0/)") 
+
+if filteringg == ib: 
+  print("ID rubika: {@restarting_Sefi}")
+  print(color.Green + "ID rubika: {@restarting_Sefi}")
+  print(color.Green + "ID rubika: {@restarting_Sefi}")
+        
+        
+        
+        
--- README.md
+++
@@ -1,1 +0,0 @@
-# Filtering-rubiks.py
